cb2bd524658a1ed9592e8dd89709b773d360af24
steven-szk <steven.szk@outlook.com>
2026-08-05 17:55:55 +0100

    change syntax

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import sendESP
 try:
     espcontrol = sendESP.sendESP()
     espcontrol.init()
-        
+    espcontrol.send_string("Initialise...")
     #serve.start_server(espcontrol)
 except Exception as e:
     print(f"Error init: {e}")    
         
 try:
-    espcontrol.send_string("Loading Camera")
     from capture import cap_frame, close_camera
+    espcontrol.send_string("Camera Loaded")
 except Exception as e:
     print(f"Error loading camera: {e}")
     
 try:
-    espcontrol.send_string("Loading Models")
     from infermodel import infer
+    espcontrol.send_string("Models Loaded")
 except Exception as e:
     print(f"Error loading models: {e}")
     
