cf6ae5ebbc7148ca35f9990ac5cc0301e80a4d13
steven-szk <steven.szk@outlook.com>
2026-08-05 17:54:16 +0100

    fix bus

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 #import serve
 import sendESP
 
-def main():
-    try:
-        espcontrol = sendESP.sendESP()
-        espcontrol.init()
+try:
+    espcontrol = sendESP.sendESP()
+    espcontrol.init()
         
-        #serve.start_server(espcontrol)
-    except Exception as e:
-            print(f"Error init: {e}")    
+    #serve.start_server(espcontrol)
+except Exception as e:
+    print(f"Error init: {e}")    
         
-    try:
-        espcontrol.send_string("Loading Camera")
-        from capture import cap_frame, close_camera
-    except Exception as e:
-            print(f"Error loading camera: {e}")
+try:
+    espcontrol.send_string("Loading Camera")
+    from capture import cap_frame, close_camera
+except Exception as e:
+    print(f"Error loading camera: {e}")
     
-    try:
-        espcontrol.send_string("Loading Models")
-        from infermodel import infer
-    except Exception as e:
-            print(f"Error loading models: {e}")
+try:
+    espcontrol.send_string("Loading Models")
+    from infermodel import infer
+except Exception as e:
+    print(f"Error loading models: {e}")
     
-    
-        
+
+
         
         
         
