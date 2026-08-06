e18ddb3ece76190352adb03c841e2ac790399b23
steven-szk <steven.szk@outlook.com>
2026-08-06 18:41:19 +0100

    lower brightness

--- capture.py
+++ capture.py
@@ -3,7 +3,7 @@ import cv2  # type: ignore
 
 # Camera config
 WIDTH, HEIGHT = 1920, 1080       # capture resolution
-EXPOSURE_US = 20000000              # shutter speed in microsections, None = auto
+EXPOSURE_US = 18000000              # shutter speed in microsections, None = auto
 '''VERY IMPORTANT, in UK, 50Hz mains, so use multiples of 10ms'''
 GAIN = 1                      # exposure conpensation
 COLOUR_GAINS = (1.2, 1.5)
